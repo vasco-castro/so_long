6e40497e4709a59ae2eac3487f144988e2ad4805
Vasco Castro <vsoares-@c1r11s11.42lisboa.com>
2025-08-08 21:20:30 +0100

    Correct targets makefile

--- targets.mk
+++ targets.mk
@@ -4,13 +4,13 @@ INCLUDES_DIR = ./inc
 MAPS_DIR = ./maps
 
 # Source files (with directories applied)
-SRCS = $(addsuffix .c,
-	$(addprefix $(SRCS_DIR)/,
+SRCS = $(addsuffix .c, \
+	$(addprefix $(SRCS_DIR)/, \
 		so_long error_handling \
 		handlers/handlers handlers/moves handlers/moves2 \
 		parsing/parsing parsing/ft_floodfill \
 		rendering/image rendering/rendering \
-	)
+	) \
 )
 
 # Object files convertion
